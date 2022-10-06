0e2434bbfd125d1136c979ddb0738d7b22a884d8
Manjula-86 <114990843+Manjula-86@users.noreply.github.com>
2022-10-06 15:24:40 +0530

    check run with new data

--- src/main/java/com/ing/demo/mvnglouddemo/MvnglouddemoApplication.java
+++ src/main/java/com/ing/demo/mvnglouddemo/MvnglouddemoApplication.java
@@ -8,6 +8,13 @@ import com.google.cloud.bigquery.BigQueryException;
 import com.google.cloud.bigquery.BigQueryOptions;
 import com.google.cloud.bigquery.Dataset;
 import com.google.cloud.bigquery.DatasetInfo;
+import com.google.cloud.bigquery.Field;
+import com.google.cloud.bigquery.Schema;
+import com.google.cloud.bigquery.StandardSQLTypeName;
+import com.google.cloud.bigquery.StandardTableDefinition;
+import com.google.cloud.bigquery.TableDefinition;
+import com.google.cloud.bigquery.TableId;
+import com.google.cloud.bigquery.TableInfo;
 
 @SpringBootApplication
 public class MvnglouddemoApplication implements CommandLineRunner {
@@ -26,9 +33,18 @@ public class MvnglouddemoApplication implements CommandLineRunner {
 	        for (int i = 0; i < args.length; ++i) {
 	        	System.out.println("args[{}]: {}"+i);
 	        }
-	        String datasetName = "MY_DATASET_NAME";
+	        String datasetName = "CUSTOMER_US";
 		    String location = "US";
-		    createDataset(datasetName,location);
+		    
+		    
+		    datasetName = createDataset(datasetName,location);
+		    String tableName = "CUSTOMER";
+		    Schema schema =
+		        Schema.of(
+		            Field.of("customername", StandardSQLTypeName.STRING),
+		            Field.of("customerid", StandardSQLTypeName.NUMERIC),
+		            Field.of("isactive", StandardSQLTypeName.BOOL));
+		    createTable(datasetName, tableName, schema);
 	    }
 	    
 	    public static String createDataset(String datasetName, String location) {
@@ -50,5 +66,24 @@ public class MvnglouddemoApplication implements CommandLineRunner {
 	    return newDatasetName;
 	   
 	  }
+	    
+	    public static void createTable(String datasetName, String tableName, Schema schema) {
+	    	System.out.println("Calling createTable method....");
+	    	try {
+	          // Initialize client that will be used to send requests. This client only needs to be created
+	          // once, and can be reused for multiple requests.
+	          BigQuery bigquery = BigQueryOptions.getDefaultInstance().getService();
+
+	          TableId tableId = TableId.of(datasetName, tableName);
+	          TableDefinition tableDefinition = StandardTableDefinition.of(schema);
+	          TableInfo tableInfo = TableInfo.newBuilder(tableId, tableDefinition).build();
+
+	          bigquery.create(tableInfo);
+	          System.out.println("Table created successfully");
+	        } catch (BigQueryException e) {
+	          System.out.println("Table was not created. \n" + e.toString());
+	        }
+	    	System.out.println("End createTable method....");
+	      }
 
 }
